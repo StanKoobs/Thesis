9750d88c1179c22190a0b8a95e1fbf8b8dd2c806
nickwkoning <nickwkoning@gmail.com>
2020-05-11 22:07:11 +0200

    split integrals for possibly slightly faster computation

--- SizeDistortionInSimpleCase.R
+++ SizeDistortionInSimpleCase.R
@@ -38,15 +38,26 @@ for (i in seq_along(N)) {
   for (j in 1:p) {
     terms = choose(p, j)
     prob_binom = dbinom(j, p, prob)
-
+    
+    # Splitting the integrals for slightly faster computation
+    integral_part_1 = integrate(f1, 
+                                lower = j * deltapn[i], 
+                                upper = Inf, 
+                                p = j)$value 
+    
+    integral_part_2 = integrate(f1, 
+                                lower = 0, 
+                                upper = j * deltapn[i], 
+                                p = j)$value
+    
     sum_lower = sum_lower + 
       terms * 
-      integrate(f1, lower = j * deltapn[i], upper = Inf, p = j)$value * 
+      integral_part_1 * 
       prob_binom
     
     sum_upper = sum_upper + 
       terms * 
-      integrate(f1, lower = 0, upper = Inf, p = j)$value * 
+      (integral_part_1 + integral_part_2) * 
       prob_binom
   }
 
